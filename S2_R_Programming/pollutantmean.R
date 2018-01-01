b455b525de3f2ef8ea5915a45f3d7f6a765cfaff
“natal”123” <15nl10@queensu.ca>
2017-12-31 23:35:00 -0500

    revised S2_R_Programming/pollutantmean.R

--- S2_R_Programming/pollutantmean.R
+++ S2_R_Programming/pollutantmean.R
@@ -6,13 +6,16 @@
 # (sulfate or nitrate) across a specified list of monitors. The function 
 # takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector 
 # monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter 
-# data from the directory specified in the 'directory' argument and returns 
-# the mean of the pollutant across all of the monitors, ignoring any missing 
-# values coded as NA.
+# data from the directory (folder: specdata) specified in the 'directory' 
+# argument and returns the mean of the pollutant across all of the monitors, 
+# ignoring any missing values coded as NA.
 
 
 pollutantmean <- function(directory, pollutant, id=1:332){
+    # Creating a vector of directory path for importing csv file 
     file_input <- paste0(directory, "/", formatC(id, width=3, flag="0"), ".csv")
+    # Dynamically loading required csv file(s) to a meta list 
+    # then collapse the list into one data.frame 
     data <- data.table::rbindlist(lapply(file_input,data.table::fread))
     if (pollutant == "sulfate"){
         return(mean(data$sulfate, na.rm = TRUE))
