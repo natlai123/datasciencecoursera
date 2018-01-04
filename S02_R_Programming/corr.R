6875e29cfac04c7ef18472a2eb5115a2cfd62051
“natal”123” <15nl10@queensu.ca>
2018-01-04 15:51:38 -0500

    Week 2

---
+++ S02_R_Programming/corr.R
@@ -0,0 +1,49 @@
+# S2_R_Programming 
+# Programming Assignment 
+# Part 3 
+
+# https://www.coursera.org/learn/r-programming/supplement/amLgW/programming-assignment-1-instructions-air-pollution
+
+# corr() takes a directory of data files and a threshold for complete cases
+# and calculates the correlation between sulfate and nitrate for monitor 
+# locations where the number of completely observed cases (on all variables) 
+# is greater than the threshold. The function should return a vector of 
+# correlations for the monitors that meet the threshold requirement. 
+# If no monitors meet the threshold requirement, then the function should 
+# return a numeric vector of length 0
+
+
+corr <- function(directory, threshold = 0){
+    # Since the function checks all files in the specdata, dynamic importing 
+    # (in part 1 and 2) is not needed here. To take all files into account, use
+    files_input <- dir(directory, full.names = TRUE)
+    dlist <- lapply(files_input, data.table::fread)
+    data <- data.table::rbindlist(dlist)
+    # remove NA items
+    data <- data[complete.cases(data)]
+    # Using data.table's feature (by=ID) to obtain obs num. and correlation within 
+    # each sublist (eg 001.csv)  then chaining (imposing constraint) [N > threshold] 
+    # to select only sublists that meet the threshold.  
+    data <- data[, .(nobs= .N, Correlation=cor(sulfate, nitrate)), by = ID][nobs > threshold]
+    return(data[, Correlation])
+}
+
+# Checking 'corr'
+
+path = "~/Desktop/datasciencecoursera/S02_R_Programming/specdata"
+
+cr <- corr(path, 150)
+head(cr)
+summary(cr)
+
+cr <- corr(path, 400)
+head(cr)
+summary(cr)
+
+cr <- corr(path, 5000)
+summary(cr)
+length(cr)
+
+cr <- corr(path)
+summary(cr)
+length(cr)
--- S02_R_Programming/pollutantmean.R
+++ S02_R_Programming/pollutantmean.R
@@ -4,13 +4,13 @@
 
 # https://www.coursera.org/learn/r-programming/supplement/amLgW/programming-assignment-1-instructions-air-pollution
 
-# The function, 'pollutantmean', calculates the mean of a pollutant 
-# (sulfate or nitrate) across a specified list of monitors. The function 
-# takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector 
-# monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter 
-# data from the directory (folder: specdata) specified in the 'directory' 
-# argument and returns the mean of the pollutant across all of the monitors, 
-# ignoring any missing values coded as NA.
+# pollutantmean() calculates the mean of a pollutant (sulfate or nitrate)
+#  across a specified list of monitors. The function  takes three arguments: 
+# 'directory', 'pollutant', and 'id'. Given a vector monitor ID numbers,
+# 'pollutantmean' reads that monitors' particulate matter data from the directory 
+# (folder: specdata) specified in the 'directory' argument and returns 
+#  the mean of the pollutant across all of the monitors, ignoring any
+#  missing values coded as NA.
 
 
 pollutantmean <- function(directory, pollutant, id=1:332){
@@ -30,6 +30,10 @@ pollutantmean <- function(directory, pollutant, id=1:332){
 # Checking 'pollutantmean'
 
 path = "~/Desktop/datasciencecoursera/S02_R_Programming/specdata"
+
 pollutantmean(directory = path, pollutant = "sulfate", 1:10) 
+
 pollutantmean(directory = path, "nitrate", 70:72)
+
 pollutantmean(directory = path, "nitrate", 23)
+
