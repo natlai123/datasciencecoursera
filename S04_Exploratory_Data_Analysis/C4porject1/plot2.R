170d7cd801dfafdd6588fb74a8de2a7bf693b15b
natlai123 <nathanielai722@yahoo.com.hk>
2018-03-02 23:27:51 +0800

    Q1&2

--- S04_Exploratory_Data_Analysis/C4porject1/plot2.R
+++ S04_Exploratory_Data_Analysis/C4porject1/plot2.R
@@ -1,6 +1,6 @@
 # S04_Exploratory_Data_Analysis
 # Assignment 1
-# Plot 1
+# Plot 2
 # Nathaniel Lai
 
 ####################################################################################################
@@ -11,7 +11,7 @@
 
 # Examine how household energy usage varies over a 2-day period in February, 2007
 
-# Constructs plot 1 and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+# Constructs plot 2 and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
 
 
 ####################################################################################################
@@ -39,17 +39,17 @@ list.files()
 #### Import UCI Electric power consumption dataset, "?" as missing values (NAs)
 powerdf <- read_delim(file="household_power_consumption.txt", 
                       delim = ";", 
-                      na = c("?", "NA"),
+                      na= c("?", "NA"),
                       col_types = cols(
-                          Date = col_date("%d/%m/%Y"),
-                          Time = col_time(format = ""),
-                          Global_active_power = col_double(),
-                          Global_reactive_power = col_double(),
-                          Voltage = col_double(),
-                          Global_intensity = col_double(),
-                          Sub_metering_1 = col_double(),
-                          Sub_metering_2 = col_double(),
-                          Sub_metering_3 = col_double()
+                        Date = col_date("%d/%m/%Y"),
+                        Time = col_time(format = ""),
+                        Global_active_power = col_double(),
+                        Global_reactive_power = col_double(),
+                        Voltage = col_double(),
+                        Global_intensity = col_double(),
+                        Sub_metering_1 = col_double(),
+                        Sub_metering_2 = col_double(),
+                        Sub_metering_3 = col_double()
                       ))
 
 #### Filter data to focus on February, 2007
@@ -57,33 +57,27 @@ powerdf <- powerdf %>%
     filter(Date >= "2007-02-01" & Date <= "2007-02-02")
 
 
-# #### Begin Exploratory Data Analysis
-# glimpse(powerdf) # classes are right
-# summary(powerdf) # same no. of NA across variables
+#### Format Timstamps 
+powerdf <- unite(powerdf, datetime, Date, Time)
+powerdf$datetime <- lubridate::ymd_hms(powerdf$datetime)
+# powerdf$datetime <- as.POSIXct(strptime(powerdf$datetime, 
+#        format="%Y-%m-%d_%H:%M:%S", tz = "GMT"))
+# powerdf$datetime<- xts(powerdf$datetime)
 
 
 #### Prepare to save to png file
-png(filename = "plot1.png", width = 480, height = 480)
+png(filename = "plot2.png", width = 480, height = 480)
 
 
 #### Construct plot 1
-hist(powerdf$Global_active_power, 
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency", 
-     col = "Red")
+plot(x = powerdf$datetime, 
+     y = powerdf$Global_active_power, 
+     type="l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "")
 
 
 dev.off()
 
 
-
-# # For a quick plot of all variables, see https://www.r-bloggers.com/quick-plot-of-all-variables/
-# # But this take some time to run ...
-# powerdf %>%
-#     keep(is.numeric) %>%
-#     gather() %>%
-#     ggplot(aes(value)) +
-#     facet_wrap(~ key, scales = "free") +
-#     geom_histogram()
 
