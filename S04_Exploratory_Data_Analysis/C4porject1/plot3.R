9c7e9f6228425178aaaa8612dcbddbf1092b4a5e
natlai123 <nathanielai722@yahoo.com.hk>
2018-03-03 10:27:47 +0800

    Q3

--- S04_Exploratory_Data_Analysis/C4porject1/plot3.R
+++ S04_Exploratory_Data_Analysis/C4porject1/plot3.R
@@ -87,7 +87,7 @@ lines(x = powerdf$datetime,
 
 legend("topright",
        col = c("black","red","blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        lty = c(1,1), 
        lwd = c(1,1))
 
