fee460abdb497538addc624dc94bd7a1a86e9ef6
natlai123 <nathanielai722@yahoo.com.hk>
2018-03-03 10:25:10 +0800

    Q3

--- S04_Exploratory_Data_Analysis/C4porject1/plot3.R
+++ S04_Exploratory_Data_Analysis/C4porject1/plot3.R
@@ -86,10 +86,10 @@ lines(x = powerdf$datetime,
       col = "blue")
 
 legend("topright",
-       col=c("black","red","blue"),
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty=c(1,1), 
-       lwd=c(1,1))
+       col = c("black","red","blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1,1), 
+       lwd = c(1,1))
 
 #### Save plot 3
 dev.off()
